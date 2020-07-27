d090ee8ea8f75810e178275490690969e1b6ab7f
mryanshenghong <mryanshenghong@gmail.com>
2020-07-27 00:50:26 -0400

    add env checking

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { CreateElement } from 'vue/types/umd'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import { CreateElement } from 'vue/types/umd';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-import './utils/element'
-import i18n from './utils/i18n'
-import './styles/reset.scss'
-import 'element-ui/lib/theme-chalk/display.css'
-import './styles/global.scss'
+import './utils/element';
+import i18n from './utils/i18n';
+import './styles/reset.scss';
+import 'element-ui/lib/theme-chalk/display.css';
+import './styles/global.scss';
 
-Vue.config.productionTip = true
-Vue.config.devtools = true
+const isDev = process.env.NODE_ENV === 'development' ? true : false;
+
+Vue.config.productionTip = isDev;
+Vue.config.devtools = isDev;
 
 new Vue({
   router,
   store,
   i18n,
   render: (h: CreateElement) => h(App),
-}).$mount('#app')
+}).$mount('#app');
