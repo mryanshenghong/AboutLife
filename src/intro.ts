1f6630aa039f632d146ef58db6fa92be97709356
mryanshenghong <mryanshenghong@gmail.com>
2022-07-20 22:14:45 -0400

    copy assets

---
+++ src/intro.ts
@@ -0,0 +1,26 @@
+export const intro = {
+  anything: {
+    title: "Anything",
+    intro: ["日常生活琐碎小事...", "吹吹牛皮...", "所见所闻...", "anything..."],
+    media_type: "img",
+    media_url: require("./assets/home.jpg"),
+  },
+  music: {
+    title: "Music",
+    intro: ["练练指弹...", "梦想有一把泰勒吉他...", "现在实现了一半那就是有了一把吉他...", "有了泰勒也是个菜逼..."],
+    media_type: "img",
+    media_url: require("./assets/intro_guitar.jpg"),
+  },
+  dota: {
+    title: "Dota",
+    intro: ["遨游在美东鱼塘...", "整天要面对不知道说些什么语言的队友...", "CNDOTA BEST DOTA...", "怀念和大家通宵Dota打话费的日子..."],
+    media_type: "img",
+    media_url: require("./assets/ti10.jpg"),
+  },
+  bambi: {
+    title: "Bambi",
+    intro: ["混血小蓝猫,看起来很老但是只有一岁半...", "贪吃,日渐涨胖...", "喜欢学鸟叫...", "每天都一副很不爽的表情..."],
+    media_type: "img",
+    media_url: require("./assets/bambi.jpg"),
+  },
+};
